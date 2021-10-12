f5ee2c5a9d571be9f31d0966406adb8d581932d2
Lapeyre Nathan <77839963+Sarolus@users.noreply.github.com>
2021-10-12 13:23:42 +0200

    Delete entity_not_found_exception.py
    The Exceptions feature is still a prototype, it will not be push until it is tested and developped.

--- exceptions/entity_not_found_exception.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-
-class EntityNotFoundException(Exception):
-
-    def __init__(self, cls, message="Not found.") -> None:
-        message = "{} entity not found.".format(cls.__class__.__name__)
-        super().__init__(message)
