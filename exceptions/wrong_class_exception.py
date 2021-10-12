828f41783fde61aca03e0e0f2985979f7aaac561
Lapeyre Nathan <77839963+Sarolus@users.noreply.github.com>
2021-10-12 13:24:00 +0200

    Delete wrong_class_exception.py
    The Exceptions feature is still a prototype, it will not be push until it is tested and developped.

--- exceptions/wrong_class_exception.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-
-class WrongClassException(Exception):
-
-    def __init__(self, obj, message="Not found.") -> None:
-        message = "{} class not found.".format(obj.__class__.__name__)
-        super().__init__(message)
