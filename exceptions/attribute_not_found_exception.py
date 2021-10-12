7c7fdb00fe64bf58baf41d129b89ddc2e7cee66a
Lapeyre Nathan <77839963+Sarolus@users.noreply.github.com>
2021-10-12 13:23:17 +0200

    Delete attribute_not_found_exception.py
    The Exceptions feature is still a prototype, it will not be push until it is tested and developped.

--- exceptions/attribute_not_found_exception.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-
-class AttributeNotFoundException(Exception):
-
-    def __init__(self, attribute: str, message="Not found.") -> None:
-        message = "Missing {}.".format(attribute)
-        super().__init__(message)
