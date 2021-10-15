6d2bdaf861fe3633d074037b8953b0b7e3dca23b
Adrien MILLOT <adrien.millot@gmail.com>
2021-10-15 08:38:04 +0200

    Add API Profile delete entrypoint

--- api/v1/views/profiles.py
+++ api/v1/views/profiles.py
@@ -62,3 +62,35 @@ def profile_show(profile_id) -> json:
         return make_response(jsonify(responseObject), 404)
 
     return make_response(jsonify(profile.to_dict()), 200)
+
+
+@app_views.route('/profiles/<profile_id>', methods=['DELETE'])
+@swag_from('documentation/profile/delete_profile.yml')
+def profile_delete(profile_id) -> json:
+    """
+        Deletes a specified Profile object.
+
+        Args:
+            profile_id : ID of the wanted Profile object.
+
+        Raises:
+            NotFound: Raises a 404 error if profile_id
+            is not linked to any Profile object.
+
+        Returns:
+            json: Empty dictionary with the status code 200.
+    """
+    profile = db_storage.get(Profile, profile_id)
+
+    if profile is None:
+        responseObject = {
+            'status': 'fail',
+            'message': 'Profile entity not found.'
+        }
+
+        return make_response(jsonify(responseObject), 404)
+
+    profile.delete()
+    db_storage.save()
+
+    return make_response(jsonify({}), 200)
--- tests/test_api/test_v1/test_views/test_profiles.py
+++ tests/test_api/test_v1/test_views/test_profiles.py
@@ -141,3 +141,68 @@ class ShowProfilesApiTest(unittest.TestCase):
         self.assertEqual(json_data['status'], 'fail')
         self.assertIn('message', json_data)
         self.assertEqual(json_data['message'], 'Profile entity not found.')
+
+
+@unittest.skipIf(getenv('SS_SERVER_MODE') != 'API', "only testing api server mode")
+class DeleteProfilesApiTest(unittest.TestCase):
+    """
+        Tests of API delete action for Profile.
+    """
+
+    def setUp(self) -> None:
+        """
+            Set up API delete action tests.
+        """
+
+        self.profile = Profile(name='toto')
+        self.profile_id = self.profile.id
+        db_storage.new(self.profile)
+        db_storage.save()
+        self.url = '{}/profiles/{}'.format(api_url, self.profile.id)
+        self.invalid_url = '{}/profiles/{}'.format(api_url, 'toto')
+
+    def tearDown(self) -> None:
+        """
+            Tear down table Profile of database used for tests.
+        """
+
+        profile = db_storage.get_from_attributes(Profile, id=self.profile.id)
+        if profile is not None:
+            db_storage.delete(profile)
+            db_storage.save()
+
+    def testDelete(self):
+        """
+            Test valid delete action.
+        """
+
+        self.assertTrue(self.profile == db_storage.get(Profile, self.profile_id))
+
+        response = requests.delete(url=self.url)
+        headers = response.headers
+
+        self.assertEqual(response.status_code, 200, WRONG_STATUS_CODE_MSG)
+        self.assertEqual(
+            headers['Content-Type'], 'application/json', WRONG_TYPE_RETURN_MSG)
+        json_data = response.json()
+        self.assertEqual(len(json_data), 0)
+        db_storage.reload()
+        self.assertIsNone(db_storage.get(Profile, self.profile_id))
+
+    def testNotFound(self):
+        """
+            Test delete action when given wrong profile_id or no ID at all.
+        """
+
+        response = requests.delete(url=self.invalid_url)
+        headers = response.headers
+
+        self.assertEqual(response.status_code, 404, WRONG_STATUS_CODE_MSG)
+        self.assertEqual(
+            headers['Content-Type'], 'application/json', WRONG_TYPE_RETURN_MSG)
+        self.assertTrue(self.profile == db_storage.get(Profile, self.profile.id))
+        json_data = response.json()
+        self.assertIn('status', json_data)
+        self.assertEqual(json_data['status'], 'fail')
+        self.assertIn('message', json_data)
+        self.assertEqual(json_data['message'], 'Profile entity not found.')
