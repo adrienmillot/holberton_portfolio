fab4267e1eef1a6be60599cb2e79488a9b80a7b0
Sarolus <cactussius.primus@gmail.com>
2021-10-20 15:38:09 +0200

    Add User Edit Web Interface

--- web_flask/static/scripts/users_list.js
+++ web_flask/static/scripts/users_list.js
@@ -119,9 +119,8 @@ function btnUserEditEvent() {
 	 * Click on edit button
 	 */
 	$('.user .btn.edit').click(function () {
-		id = $(this).attr('data-id');
-		localStorage.setItem('edit_user_id', id)
-		window.location = '/user_edit'
+		const user_id = $(this).attr('data-id');
+		window.location = '/users/' + user_id + '/edit';
 	});
 
 }
