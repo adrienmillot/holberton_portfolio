3b8f8b363397713ad23df6e7dfcae90b0370886d
Sarolus <cactussius.primus@gmail.com>
2021-10-31 11:31:59 +0100

    Add API Category all user score by survey entrypoint

--- api/v1/views/categories.py
+++ api/v1/views/categories.py
@@ -6,6 +6,7 @@
 from math import ceil
 from api.v1.views import app_views
 from models.category import Category
+from models.survey import Survey
 from models.user import User
 from models import db_storage
 from flask import abort, jsonify, make_response, request
@@ -232,3 +233,48 @@ def all_category_user_score():
     }
 
     return make_response(jsonify(responseObject), 200)
+
+
+@app_views.route('/surveys/<survey_id>/categories_score', methods=['GET'], strict_slashes=False)
+@swag_from('documentation/category/survey_categories.yml')
+def survey_categories_score(survey_id):
+    """
+        Show the score on all categories of a survey for a specified user.
+    """
+    headers = request.headers
+    auth_token = headers['Authorization'].split(' ')[1]
+    user_id = User.decode_auth_token(auth_token)
+    categories_max_score = dict(db_storage.survey_categories_max_score(survey_id, user_id))
+    categories_user_score = dict(db_storage.survey_categories_user_score(survey_id, user_id))
+    survey = db_storage.get(Survey, survey_id)
+
+    if survey is None:
+        responseObject = {
+            'status': 'fail',
+            'message': 'Survey entity not found.'
+        }
+
+        return make_response(jsonify(responseObject), 404)
+
+    for key, value in categories_max_score.items():
+        if key not in categories_user_score:
+            categories_user_score.update({key: 0})
+
+    survey.max_score = categories_max_score
+    survey.user_score = categories_user_score
+
+    if user_id is None:
+        responseObject = {
+            'status': 'fail',
+            'message': 'User entity not found.'
+        }
+
+        return make_response(jsonify(responseObject), 404)
+    
+    responseObject = {
+        'status': 'succes',
+        'survey': survey.to_dict()
+    }
+
+    return make_response(jsonify(responseObject), 200)
+
